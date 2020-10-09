0eb13d04f22a40eadb9792cacea02bf130a49e60
tcjohnosn2 <terrence.c.johnson2@gmail.com>
2020-10-09 17:23:29 -0500

    Restart button works. Still working on playMove function.

--- Project-1/connect.js
+++ Project-1/connect.js
@@ -8,6 +8,9 @@ const $welcome = $('#welcome');
 //Accessing the close button of pop up
 const $closeBtn = $('#close');
 
+//Accessing refresh button
+const $resetBtn = $('#reset');
+
 //Accessing individual board pieces //=============> Play a piece no good <============\\
 // const $circle = $('.circle');
 
@@ -19,6 +22,13 @@ const openWlecome = () => {
 //Event handler for closing pop up window
 const closeWelcome = () => {
     $welcome.css('display', 'none');
+}
+
+//Event handler for refresh button
+const refreshPage = () => {
+    $resetBtn.click(function(){
+        location.reload(true)
+    })
 }
 
 // Function to click the board pieces //=============> Play a piece no good <============\\
@@ -35,7 +45,7 @@ $closeBtn.on('click', closeWelcome);
 //Event listener for playing an individual piece //==========> Play a piece no good <==========\\
 // $circle.on('click', playMove)
 
-
+$resetBtn.on('click', refreshPage)
 
 
 });
